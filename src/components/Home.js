6f35bc8e5cc9149cd002a8d7da9527675c5a83f7
Mallarie <mallariemilano@gmail.com>
2019-06-05 16:06:45 -0700

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateMeme from '../containers/memes/CreateMeme';
+import AllMemes from '../containers/memes/AllMemes';
+
+export default function Home() {
+  return (
+    <section>
+      <CreateMeme />
+      <AllMemes />
+    </section>
+  );
+}
