2a8b8b342b7e34675b09852d301d31dd9ec5adc6
Mallarie <mallariemilano@gmail.com>
2019-06-05 15:55:26 -0700

    Added router, switch,a nd route to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
   return (
-    <h1>Hi</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
