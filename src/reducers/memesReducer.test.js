c5711a53184b1b4894e355d67f3785f0b35aeb0b
Mallarie <mallariemilano@gmail.com>
2019-06-05 13:43:32 -0700

    Failing reducer test

---
+++ src/reducers/memesReducer.test.js
@@ -0,0 +1,21 @@
+import reducer from './memesReducer';
+import { createMeme } from '../actions/memes/MemesActions';
+
+describe('meme reducer', () => {
+  it('returns an empty array on initialization', () => {
+    const newState = reducer(undefined, {
+      type: '@@INIT'
+    });
+
+    expect(newState).toEqual([]);
+  });
+
+  it('handles the create meme action', () => {
+    const newState = reducer([], createMeme('hola'));
+
+    expect(newState).toEqual([
+      { url: 'hola' }
+    ]);
+  });
+});
+
