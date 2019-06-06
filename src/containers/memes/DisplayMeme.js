d2710660e7f5f3351ea23b9b5268bef6e7aef5ee
Mallarie <mallariemilano@gmail.com>
2019-06-06 09:31:43 -0700

    displayMeme container

---
+++ src/containers/memes/DisplayMeme.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+const mapStateToProps = (state, props) => ({
+  meme: {
+    id: props.match.params.memeId,
+    url: state.memes[props.match.params.memeId].url
+  }
+});
+
+export default withRouter(connect(
+  mapStateToProps
+));
