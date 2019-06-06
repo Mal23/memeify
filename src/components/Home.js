4018f99460a9376838f4b3048bcd88fca469b955
Mallarie <mallariemilano@gmail.com>
2019-06-06 10:59:13 -0700

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CreateMeme from '../containers/memes/CreateMeme';
 import AllMemes from '../containers/memes/AllMemes';
+import styles from './styles.css';
 
 export default function Home() {
   return (
-    <section>
+    <section className={styles.Home}>
       <CreateMeme />
       <AllMemes />
     </section>
--- src/components/MemePage.js
+++ src/components/MemePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import DisplayMeme from '../containers/memes/DisplayMeme';
 import { Link } from 'react-router-dom';
+import styles from './styles.css';
 
 export default function MemePage() {
   return (
-    <section>
+    <section className={styles.MemePage}>
       <Link to="/"><p>Home</p></Link>
       <DisplayMeme />
     </section>
