d326acb9bb7d8856ed85d3b64c13bb25557dc6c4
Mallarie <mallariemilano@gmail.com>
2019-06-06 14:04:52 -0700

    scapping project

--- src/actions/memes/MemesActions.js
+++
@@ -1,5 +0,0 @@
-export const CREATE_MEME = 'CREATE_MEME';
-export const createMeme = url => ({
-  type: CREATE_MEME,
-  payload: { url }
-});
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import memes from './memesReducer';
-
-export default combineReducers({
-  memes
-});
