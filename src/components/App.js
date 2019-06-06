75d967702dd0e353c85b1e38df13960795fa6dac
Mallarie <mallariemilano@gmail.com>
2019-06-06 10:08:04 -0700

    added memeId

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import {
   Route
 } from 'react-router-dom';
 import Home from './Home';
+import MemePage from './MemePage';
 
 export default function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/:memeId" component={MemePage} />
         <Route exact path="/" component={Home} />
       </Switch>
     </Router>
